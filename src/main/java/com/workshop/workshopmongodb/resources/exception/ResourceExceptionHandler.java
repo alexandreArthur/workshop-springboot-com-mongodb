1cd5b19fd018854d1aaef85cd0a16d16252f2e46
alexandreArthur <arthur_dda@hotmail.com>
2021-09-22 19:16:29 -0300

    Obtendo usuario por id

---
+++ src/main/java/com/workshop/workshopmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.workshop.workshopmongodb.resources.exception;
+
+import com.workshop.workshopmongodb.service.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest re){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(),status.value(), "Não encontrado",e.getMessage(),re.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+}
